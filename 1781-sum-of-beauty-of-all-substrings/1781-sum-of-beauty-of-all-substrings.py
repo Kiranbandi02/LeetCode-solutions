89da552d3295794f38bf0ff34c1adb5b565db1a9
KIRAN BANDI <95462023+Kiranbandi02@users.noreply.github.com>
2023-07-09 22:53:20 +0530

    Time: 12494 ms (27.39%), Space: 16.2 MB (67.22%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
@@ -6,9 +6,7 @@ class Solution:
         n=len(s)
         for i in range(len(s)):
             for j in range(i+1,n+1):
-                l.append(s[i:j])
-        for i in l:
-            c=Counter(i)
-            sum+=(max(c.values())-min(c.values()))
+                c=Counter(s[i:j])
+                sum+=(max(c.values())-min(c.values()))
         return sum
                 
