876345ae9d551a2bd7b48a2129d589d63e5853a0
KIRAN BANDI <95462023+Kiranbandi02@users.noreply.github.com>
2023-08-13 22:36:02 +0530

    Time: 852 ms (93.08%), Space: 92.3 MB (40.77%) - LeetHub

---
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def validPartition(self, nums: List[int]) -> bool:
+        n = len(nums)
+        memo = {-1: True}
+        def prefixIsValid(i):
+            if i in memo:
+                return memo[i]
+            ans = False
+            if i > 0 and nums[i] == nums[i - 1]:
+                ans |= prefixIsValid(i - 2)
+            if i > 1 and nums[i] == nums[i - 1] == nums[i - 2]:
+                ans |= prefixIsValid(i - 3)
+            if i > 1 and nums[i] == nums[i - 1] + 1 == nums[i - 2] + 2:
+                ans |= prefixIsValid(i - 3)
+            memo[i] = ans
+            return ans
+
+        return prefixIsValid(n - 1)
